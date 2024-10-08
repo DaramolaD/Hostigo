6666c5ce119f2d0c42c6f09743c6341d75caf9cc
Daniel <daniel.daramola@localtechnocrat.com>
2024-10-08 21:05:17 +0100

    Implement fallback redirect for non-API routes to frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -43,8 +43,9 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")))
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/my-hostels", myHostelRoutes);
-app.get("/test/api", async (req: Request, res: Response) => {
-  res.json("Hello there from servicer");
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 
 // Define custom error classes
